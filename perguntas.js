d6174028e5b22a4d17ac4d816059f12afa713dd5
Nico15032007 <168036664+Nico15032007@users.noreply.github.com>
2024-11-19 07:28:20 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual é a raiz de 144?',
+    'A raiz de 144 é 12'
+)
+
+criaCartao(
+    'História',
+    'Quando se iniciou a guerra dos canudos?',
+    'Em 7 de novembro de 1896'
+)
+
+criaCartao(
+    'Física',
+    'A água precisa estar em quantos graus para evaporar?',
+    'A água começa evaporar quando atinge a temperatuda de 100 graus'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são as camadas da Terra?',
+    'Crosta terrestre, manto e núcleo'
+)
